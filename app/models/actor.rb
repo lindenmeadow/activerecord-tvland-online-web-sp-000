7dc295ed22d1592c3483617ad98d8828e7870312
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-24 21:36:48 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,5 +6,10 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
+  def list_roles
+    self.characters.collect do |char|
+        "#{char.name} - #{char.show.name}"
+    end
+  end
 
 end
